925640bbf7081bab1d7645fbb9fe269a211a42e7
katcze91 <91422768+katcze91@users.noreply.github.com>
2022-05-07 14:51:20 +0200

    updated css

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer(){
+    return (
+      <footer>
+        <a href="https://github.com/katcze91/weather-react">Opensourced</a> by
+        Katarzyna Czepiela
+      </footer>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Footer from './Footer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Weather search engine!</h1>
+    <h1>Weather search engine</h1>
     <App />
+    <Footer/>
   </React.StrictMode>
 );
 
