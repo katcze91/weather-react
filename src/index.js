60005faef12207dad8f0ff417c7a858d3feff30f
katcze91 <91422768+katcze91@users.noreply.github.com>
2022-05-07 13:52:14 +0200

    build deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Weather search engine</h1>
+    <h1>Weather search engine!</h1>
     <App />
   </React.StrictMode>
 );
