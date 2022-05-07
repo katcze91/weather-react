51eb59af171eceb307b9dee8df5d0cea2edc8b97
katcze91 <91422768+katcze91@users.noreply.github.com>
2022-05-07 12:50:02 +0200

    Weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  function handleResponse(response){
+    alert(`The weather in Madrid is ${response.data.main.temp}`)
+  }
+  let apiKey = "c49eff189373b6b9c4fea0abf89e5033";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=${apiKey}`; 
+  axios.get(apiUrl).then(handleResponse)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2> Hello world</h2>
     </div>
   );
 }
