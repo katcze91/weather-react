d68d55551f37287354b38e1e3a04620305655c61
katcze91 <91422768+katcze91@users.noreply.github.com>
2022-05-21 12:12:46 +0200

    only celcius temperature displayed

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,40 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celcius");
-  function convertToFarenheit(event) {
-    event.preventDefault();
-    setUnit("farenheit");
-  }
-  function convertToCelcius(event) {
-    event.preventDefault();
-    setUnit("celcius");
-  }
-  if (unit === "celcius") {
+  
     return (
       <div className="WeatherTemperature">
         <span className="temperature">{Math.round(props.celcius)}</span>
         <span className="unit">
-          °C I{" "}
-          <a href="/" onClick={convertToFarenheit}>
-            °F{" "}
-          </a>
+          °C 
+         
+          
         </span>
       </div>
-    );
-  } else {
-    let farenheit = (props.celcius * 9) / 5 + 32;
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(farenheit)}</span>
-        <span className="unit">
-          <a href="/" onClick={convertToCelcius}>
-            °C{" "}
-          </a>
-          I °F
-        </span>
-      </div>
-    );
+    )
   }
-}
+
